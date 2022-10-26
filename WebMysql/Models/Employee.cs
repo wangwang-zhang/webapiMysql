78048e13ff53fa0dbfbb2a6269b3a1cd7f3464b2
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-10-26 17:07:49 +0800

    Added a model Employee

---
+++ WebMysql/Models/Employee.cs
@@ -0,0 +1,10 @@
+namespace WebMysql.Models;
+
+public class Employee
+{
+    public int EmployeeId { get; set; }
+    public string EmployeeName { get; set; }
+    public string Department { get; set; }
+    public string DateOfjoining { get; set; }
+    public string PhotoFileName { get; set; }
+}
