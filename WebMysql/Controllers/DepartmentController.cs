bd5922f07b34fe8b926c112ced400f30a125a255
wangwang.zhang <wangwang.zhang@thoughtworks.com>
2022-10-26 13:59:13 +0800

    Implemented a new webapi to insert data to database

--- WebMysql/Controllers/DepartmentController.cs
+++ WebMysql/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
+using WebMysql.Models;
 
 namespace WebMysql.Controllers;
 
@@ -36,5 +37,28 @@ public class DepartmentController : Controller
             }
         }
         return new JsonResult(table);
+    }
+    [HttpPost]
+    public JsonResult Post(Department department)
+    {
+        string query = @"
+          insert into Department(DepartmentName) values 
+                        (@DepartmentName);
+        ";
+        DataTable table = new DataTable();
+        string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+        using (MySqlConnection myCon = new MySqlConnection(sqlDataSource))
+        {
+            myCon.Open();
+            using (MySqlCommand myCommand = new MySqlCommand(query, myCon))
+            {
+                myCommand.Parameters.AddWithValue("@DepartmentName", department.DepartmentName);
+                var myReader = myCommand.ExecuteReader();
+                table.Load(myReader);
+                myReader.Close();
+                myCon.Close();
+            }
+        }
+        return new JsonResult("Added successfully");
     }
 }
